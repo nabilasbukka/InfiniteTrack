8b7f5da578918462076601251e697893fa98341e
Nabila Putri Syafrina Bukka <nabilasyafrina.business@gmail.com>
2024-09-26 10:57:06 +0700

    Initial commit

---
+++ InfiniteTrack/InfiniteTrack/InfiniteTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteTrackApp.swift
+//  InfiniteTrack
+//
+//  Created by Nabila Syafrina on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
